cb92e80bcec3aa84b0025418eee56ee6c172dc80
Hau HP <hauhpdn2015@gmail.com>
2021-04-29 10:28:30 +0700

    bt

---
+++ src/bai1_introduction_to_java/bai_tap_1/hien_thi_loi_chao.java
@@ -0,0 +1,10 @@
+package bai1_introduction_to_java.bai_tap_1;
+import java.util.Scanner;
+public class hien_thi_loi_chao {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter your name:");
+       String name = sc.nextLine();
+       System.out.println("Hello: "+ name);
+    }
+}
