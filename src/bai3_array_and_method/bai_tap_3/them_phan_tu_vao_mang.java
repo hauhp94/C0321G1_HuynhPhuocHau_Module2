9ce234119ab9bffc436f1df14cdaac876d76dae8
Hau HP <hauhpdn2015@gmail.com>
2021-05-03 15:24:04 +0700

    bt

---
+++ src/bai3_array_and_method/bai_tap_3/them_phan_tu_vao_mang.java
@@ -0,0 +1,29 @@
+package bai3_array_and_method.bai_tap_3;
+
+import java.util.Scanner;
+
+public class them_phan_tu_vao_mang {
+    public static void main(String[] args) {
+        int[] array = {10, 4, 6, 7, 8, 0, 0, 0, 0, 0};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập phần tử muốn thêm");
+        int x = input.nextInt();
+        Scanner inputIndex = new Scanner(System.in);
+        System.out.println("Nhập vị trí muốn chèn");
+        int index = inputIndex.nextInt();
+        if (index <= 1 || index >= array.length) {
+            System.out.println("không chèn được");
+        } else {
+            for (int i = array.length-1; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = x;
+        }
+        System.out.print("Mảng sau khi thêm: ");
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+
+
+    }
+}
