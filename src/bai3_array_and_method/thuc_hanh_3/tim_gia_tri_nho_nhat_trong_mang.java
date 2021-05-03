83cd420951e4798ac09a8962221cd2508fed3d74
Hau HP <hauhpdn2015@gmail.com>
2021-05-03 10:51:13 +0700

    th

---
+++ src/bai3_array_and_method/thuc_hanh_3/tim_gia_tri_nho_nhat_trong_mang.java
@@ -0,0 +1,19 @@
+package bai3_array_and_method.thuc_hanh_3;
+
+public class tim_gia_tri_nho_nhat_trong_mang {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
