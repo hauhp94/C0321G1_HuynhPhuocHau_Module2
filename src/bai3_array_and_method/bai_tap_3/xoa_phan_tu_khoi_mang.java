01fd414288ab4a48d5f8993ed34ae4124b878aa9
Hau HP <hauhpdn2015@gmail.com>
2021-05-03 11:11:17 +0700

    bt

---
+++ src/bai3_array_and_method/bai_tap_3/xoa_phan_tu_khoi_mang.java
@@ -0,0 +1,25 @@
+package bai3_array_and_method.bai_tap_3;
+
+import java.util.Scanner;
+
+public class xoa_phan_tu_khoi_mang {
+    public static void main(String[] args) {
+        int[] array = {10,4,6,7,8,6,0,0,0,0};
+        System.out.println("Mảng trước khi xóa: "+ "10,4,6,7,8,6,0,0,0,0");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập phần tử muốn xóa");
+        int x = input.nextInt();
+        for (int i=0;i<array.length;i++){
+            if(array[i]==x){
+                array[i]=array[i+1];
+                for (int j=i;j<array.length-1;j++){
+                    array[j]=array[j+1];
+                }
+            }
+        }
+        System.out.println("Mảng sau khi xóa");
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+ " ");
+        }
+    }
+}
