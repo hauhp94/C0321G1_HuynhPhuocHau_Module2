cef5b513918a793c19fb7145e9468680618b486b
Hau HP <hauhpdn2015@gmail.com>
2021-05-07 15:44:55 +0700

    bt

--- src/bai5_access_modifier_static_method_static_property/bai_tap_5/Circle.java
+++ src/bai5_access_modifier_static_method_static_property/bai_tap_5/Circle.java
@@ -1,31 +1,19 @@
 package bai5_access_modifier_static_method_static_property.bai_tap_5;
 
 public class Circle {
-    private double radius = 1.0;
+    private double radius = 1;
     private String color = "red";
-    public Circle(){
-        this.radius = 1.0;
-        this.color="red";
+    protected Circle(){
     }
-    public Circle(double radius, String color){
-        this.radius = radius;
-        this.color = color;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setRadius(double radius) {
+    protected Circle(double radius){
         this.radius = radius;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    protected double getRadius() {
+        return this.radius;
     }
 
+    protected double getArea(){
+        return Math.PI*this.radius*this.radius ;
+    }
 }
--- src/bai5_access_modifier_static_method_static_property/bai_tap_5/TestCircle.java
+++ src/bai5_access_modifier_static_method_static_property/bai_tap_5/TestCircle.java
@@ -2,9 +2,10 @@ package bai5_access_modifier_static_method_static_property.bai_tap_5;
 
 public class TestCircle {
     public static void main(String[] args) {
-    Circle c1 = new Circle();
-    System.out.println(c1.getColor()+" "+ c1.getRadius());
-    Circle c2 = new Circle(2.0,"blue" );
-        System.out.println(c2.getColor()+ " "+ c2.getRadius());
-}
+        Circle c1 = new Circle();
+        System.out.println("Area: " + c1.getArea() + " Radius: " + c1.getRadius());
+        Circle c2 = new Circle(2.0);
+        System.out.println("Area: " + c2.getArea() + " Radius: " + c2.getRadius());
+
+    }
 }
