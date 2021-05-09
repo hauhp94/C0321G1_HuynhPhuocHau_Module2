14f474de746c6e3e23aabc647de05058a5980de6
Hau HP <hauhpdn2015@gmail.com>
2021-05-09 16:14:57 +0700

    th

---
+++ src/bai7_abstract_class_interface/thuc_hanh_7/comparable/ComparableCircle.java
@@ -0,0 +1,25 @@
+package bai7_abstract_class_interface.thuc_hanh_7.comparable;
+
+import bai6_ke_thua.thuc_hanh_6.Circle;
+
+public class ComparableCircle extends Circle
+        implements Comparable<ComparableCircle> {
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
