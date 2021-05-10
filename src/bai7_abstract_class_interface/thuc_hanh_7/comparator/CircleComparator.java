d6e5871a2478a08c04b458f0ed3f8a5a990f2b18
Hau HP <hauhpdn2015@gmail.com>
2021-05-10 07:11:40 +0700

    th

---
+++ src/bai7_abstract_class_interface/thuc_hanh_7/comparator/CircleComparator.java
@@ -0,0 +1,15 @@
+package bai7_abstract_class_interface.thuc_hanh_7.comparator;
+
+import bai6_ke_thua.thuc_hanh_6.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
