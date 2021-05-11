c7e7b811ef8b1ec0ee7799965fd4a8a5c9e1dcc3
Hau HP <hauhpdn2015@gmail.com>
2021-05-11 09:34:50 +0700

    th

---
+++ src/bai9_automated_testing_tdd/thuc_hanh_9/caculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package bai9_automated_testing_tdd.thuc_hanh_9.caculator;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
