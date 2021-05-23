2330b216fb8b5d910470d265dcc6559dd938a91c
Hau HP <hauhpdn2015@gmail.com>
2021-05-23 11:07:14 +0700

    bt

--- src/bai18_string_regex/bai_tap_18/validate_so_dien_thoai/PhoneNumberTest.java
+++ src/bai18_string_regex/bai_tap_18/validate_so_dien_thoai/PhoneNumberTest.java
@@ -2,6 +2,8 @@ package bai18_string_regex.bai_tap_18.validate_so_dien_thoai;
 
 import bai18_string_regex.bai_tap_18.validate_ten_lop_hoc.NameClass;
 
+import java.util.Scanner;
+
 public class PhoneNumberTest {
     public static PhoneNumber phoneNumber;
 
@@ -10,5 +12,10 @@ public class PhoneNumberTest {
         System.out.println(phoneNumber.validate("(45)-(0935256851)"));
         System.out.println(phoneNumber.validate("(84)935256851"));
         System.out.println(phoneNumber.validate("(84)-5935256851"));
+        System.out.println("Nhập số điện thoại để kiểm tra: ");
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+        System.out.println(number+" hợp lệ? "+ phoneNumber.validate(number));
+
     }
 }
