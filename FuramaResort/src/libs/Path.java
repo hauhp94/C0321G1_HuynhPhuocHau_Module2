5d5f916c1cc850cd476a057aa4b91cd57e748afc
Hau HP <hauhpdn2015@gmail.com>
2021-06-03 19:50:03 +0700

    bt

--- FuramaResort/src/libs/Path.java
+++ FuramaResort/src/libs/Path.java
@@ -2,11 +2,11 @@ package libs;
 
 public class Path {
     //    Chứa đường dẫn các file csv
-    public static final String PATH_CUSTOMER_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Customer.csv";
-    public static final String PATH_VILLA_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Villa.csv";
-    public static final String PATH_HOUSE_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\House.csv";
-    public static final String PATH_ROOM_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Room.csv";
-    public static final String PATH_SERVICES_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Services.csv";
-    public static final String PATH_EMPLOYEE_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Employee.csv";
-    public static final String PATH_BOOKING_CSV = "D:\\C0321G1_HuynhPhuocHau_Module2\\FuramaResort\\src\\data\\Booking.csv";
+    public static final String PATH_CUSTOMER_CSV = "src/data/Customer.csv";
+    public static final String PATH_VILLA_CSV = "src/data/Villa.csv";
+    public static final String PATH_HOUSE_CSV = "src/data/House.csv";
+    public static final String PATH_ROOM_CSV = "src/data/Room.csv";
+    public static final String PATH_SERVICES_CSV = "src/data/Services.csv";
+    public static final String PATH_EMPLOYEE_CSV = "src/data/Employee.csv";
+    public static final String PATH_BOOKING_CSV = "src/data/Booking.csv";
 }
